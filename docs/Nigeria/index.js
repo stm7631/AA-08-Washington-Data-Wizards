80283f41bdaadb13a27f8151dc10ba21157b67cb
Bradley Balansay <bradleybalansay@gmail.com>
2020-05-14 23:03:12 -0700

    Added loading period to avoid errors in viz

--- docs/Nigeria/index.js
+++ docs/Nigeria/index.js
@@ -1,13 +1,24 @@
-$('#diseases').on('change', function () {
-    if (this.value == "oncho") {
-        $('#onchoViz').removeClass("hidden")
-        $('#schistoViz, #lfViz').addClass("hidden")
-    } else if (this.value == "schisto") {
-        $('#schistoViz').removeClass("hidden")
-        $('#onchoViz, #lfViz').addClass("hidden")
-    } else {
-        $('#lfViz').removeClass("hidden")
-        $('#onchoViz, #schistoViz').addClass("hidden")
-    }
-});
-  
+// Weird bug occurs if I don't set a timer
+$('#onchoViz').addClass("noviz")
+$('html').addClass("noScroll")
+
+setTimeout(() => {
+    $('#schistoViz, #lfViz').addClass("hidden")
+    $('#onchoViz').removeClass("noviz")
+
+    $('#diseases').on('change', function () {
+        if (this.value == "oncho") {
+            $('#onchoViz').removeClass("hidden")
+            $('#schistoViz, #lfViz').addClass("hidden")
+        } else if (this.value == "schisto") {
+            $('#schistoViz').removeClass("hidden")
+            $('#onchoViz, #lfViz').addClass("hidden")
+        } else {
+            $('#lfViz').removeClass("hidden")
+            $('#onchoViz, #schistoViz').addClass("hidden")
+        }
+    });
+
+    $('html').removeClass("noScroll")
+    $('.cover, .loader').remove()
+}, 10000);
