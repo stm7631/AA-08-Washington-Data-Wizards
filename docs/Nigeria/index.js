a8ca74559e2806cc022bfbe9ed86d9b4805c1c23
Bradley Balansay <bradleybalansay@gmail.com>
2020-05-20 15:36:27 -0700

    Added more dropdowns

--- docs/Nigeria/index.js
+++ docs/Nigeria/index.js
@@ -6,15 +6,15 @@ setTimeout(() => {
 
     $('#diseases').on('change', function () {
         if (this.value == "oncho") {
-            $('#dataInfo').text("Data Information (Oncho)")
+            $('#dataInfo').text("Oncho Specific Information")
             $('#onchoSummary, #onchoData, #onchoViz').removeClass("hidden")
             $('#schistoSummary, #schistoData, #schistoViz, #lfSummary, #lfData, #lfViz').addClass("hidden")
         } else if (this.value == "schisto") {
-            $('#dataInfo').text("Data Information (Schisto)")
+            $('#dataInfo').text("Schisto Specific Information")
             $('#schistoSummary, #schistoData, #schistoViz').removeClass("hidden")
             $('#onchoSummary, #onchoData, #onchoViz, #lfSummary, #lfData, #lfViz').addClass("hidden")
         } else {
-            $('#dataInfo').text("Data Information (LF)")
+            $('#dataInfo').text("LF Specific Information")
             $('#lfSummary, #lfData, #lfViz').removeClass("hidden")
             $('#onchoSummary, #onchoData, #onchoViz, #schistoSummary, #schistoData, #schistoViz').addClass("hidden")
         }
